78137b8a9121e6ca1cab7835e2ac79fc89c7c4ce
emurdock1101 <emurdock1101@gmail.com>
2020-02-19 12:52:00 -0500

    added sound, cleaned up myGame

--- src/main/MyGame.cpp
+++ src/main/MyGame.cpp
@@ -15,42 +15,13 @@ MyGame::MyGame() : Game(1200, 1000) {
 	// move that point to the middle
 	allSprites->position = {600, 500};
 	//instance->addChild(allSprites);
-
-	sun = new AnimatedSprite("sun");
-	sun->addAnimation("./resources/solarSystem/", "Sun", 4, 2, true);
-	sun->play("Sun");
-	// cout << sun->getWidth() << sun->getHeight();
-	sun->position = {0, 0};
-	sun->width = sun->height = 100;
-	sun->pivot = {50, 50};
-	allSprites->addChild(sun);
-
-	p1container = new DisplayObjectContainer();
-	p2container = new DisplayObjectContainer();
-	sun->addChild(p1container);
-	sun->addChild(p2container);
-
-	// planet1 = new Sprite("planet1","./resources/solarSystem/Planet.png");
-	// planet1->position = {200, 0};
-	// planet1->width = planet1->height = 30;
-	// planet1->pivot = {15, 15};
-	// p1container->addChild(planet1);
-
-	planet2 = new Sprite("planet2","./resources/solarSystem/Planet.png");
-	planet2->position = {300, 0};
-	planet2->width = planet2->height = 30;
-	planet2->pivot = {15, 15};
-	p2container->addChild(planet2);
-
-	moon1_1 = new Sprite("moon1_1", "./resources/solarSystem/Moon.png");
-	moon1_1->position = {50, 0};
-	moon1_1->width = moon1_1->height = 15;
-	planet1->addChild(moon1_1);
 	
+	//loads DisplayObjects from scene text files
 	sc = new Scene();
 	sc->loadScene("./resources/scenes/Scene1.txt");
 	instance->addChild(sc);
 
+	//plays background music upon launch
 	sound = new Sound();
 	sound->playMusic("./resources/sounds/boss.ogg");
 }
@@ -58,45 +29,9 @@ MyGame::MyGame() : Game(1200, 1000) {
 MyGame::~MyGame(){
 }
 
-
 void MyGame::update(set<SDL_Scancode> pressedKeys){
-	if (pressedKeys.find(SDL_SCANCODE_RIGHT) != pressedKeys.end()) {
-		sun->position.x += 2;
-	}
-	if (pressedKeys.find(SDL_SCANCODE_LEFT) != pressedKeys.end()) {
-		sun->position.x -= 2;
-	}
-	if (pressedKeys.find(SDL_SCANCODE_DOWN) != pressedKeys.end()) {
-		sun->position.y += 2;
-	}
-	if (pressedKeys.find(SDL_SCANCODE_UP) != pressedKeys.end()) {
-		sun->position.y -= 2;
-	}
-	if (pressedKeys.find(SDL_SCANCODE_A) != pressedKeys.end()) {
-		// sun->rotation += 0.01;
-		p1container->rotation += 0.05;
-		p2container->rotation += 0.03;
-		planet1->rotation += 0.1;
-		p1container->position.x = 100*sin(p1container->rotation);
-		p2container->position.x = 100*sin(p2container->rotation);
-	}
-	if (pressedKeys.find(SDL_SCANCODE_S) != pressedKeys.end()) {
-		// sun->rotation -= 0.01;
-		p1container->rotation -= 0.05;
-		p2container->rotation -= 0.03;
-		planet1->rotation -= 0.1;
-		p1container->position.x = 100*sin(p1container->rotation);
-		p2container->position.x = 100*sin(p2container->rotation);
-	}
-	if (pressedKeys.find(SDL_SCANCODE_Q) != pressedKeys.end()) {
-		allSprites->scaleX *= 1.05;
-		allSprites->scaleY *= 1.05;
-	}
-	if (pressedKeys.find(SDL_SCANCODE_W) != pressedKeys.end()) {
-		allSprites->scaleX *= 1/1.05;
-		allSprites->scaleY *= 1/1.05;
-	}
-	if (pressedKeys.find(SDL_SCANCODE_P) != pressedKeys.end()) {
+	
+	if (pressedKeys.find(SDL_SCANCODE_P) != pressedKeys.end()) {//press P to switch scene
 		if(t){
 			sc->loadScene("./resources/scenes/Scene2.txt");
 			t = false;
@@ -105,19 +40,12 @@ void MyGame::update(set<SDL_Scancode> pressedKeys){
 			sc->loadScene("./resources/scenes/Scene1.txt");
 			t = true;
 		}
-		sun->play("Sun");
 	}
-	if (pressedKeys.find(SDL_SCANCODE_L) != pressedKeys.end()) {
-		sun->stop();
-	}
-	if (pressedKeys.find(SDL_SCANCODE_G) != pressedKeys.end()) {
+
+	if (pressedKeys.find(SDL_SCANCODE_G) != pressedKeys.end()) {//press G to play sound
 		sound->playSFX("./resources/sounds/TORYAH.wav");
 	}
-	if (pressedKeys.find(SDL_SCANCODE_H) != pressedKeys.end()) {
-		if (Mix_PlayingMusic() == 0) {
-			sound->playMusic("./resources/sounds/boss.ogg");
-		}
-	}
+
 	Game::update(pressedKeys);
 }
 
